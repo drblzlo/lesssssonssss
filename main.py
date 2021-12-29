2d8c6f39362820e75992d508135f94ac17fd309b
Yuriy Zlotnikov <yzlo.it@gmail.com>
2021-12-28 21:09:55 -0600

    TODO added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
+# Press Shift+F10 /to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+#TODO Sleep more
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +12,8 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+
+print(f'')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
